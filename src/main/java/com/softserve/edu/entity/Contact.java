a0a97a4f73e213e31fdf8af71141187e837d02aa
Jaroslav <garasym@gmail.com>
2022-09-15 19:12:36 +0300

    Spring MVC. Example

---
+++ src/main/java/com/softserve/edu/entity/Contact.java
@@ -0,0 +1,71 @@
+package com.softserve.edu.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="contacts")
+public class Contact {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+    	System.out.println("***setFirstname: " + firstname);
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+}
